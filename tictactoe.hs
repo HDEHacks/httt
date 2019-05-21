7b5a52ae38b4508b2fe29d73dca2a457f132ca23
Hedon <jdmaravelias@gmail.com>
2019-05-20 20:37:16 -0500

    Finish board and display logic

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,6 +1,7 @@
 data Piece = Cross | Naught deriving (Show, Eq)
-type Board = [(Maybe Piece)]
+type Board = [Maybe Piece]
 
+-- Board logic: this is monadic
 emptyBoard :: Board
 emptyBoard = take 9 $ repeat Nothing
 
@@ -17,7 +18,27 @@ zones [a,b,c,
                  [a,d,g], [b,e,h], [c,f,i],
                  [a,e,i], [g,e,c]]
 
--- winner :: Board -> Bool
--- winner b = let possibilities = zones b
---                filled = filter (\x -> not (elem Nothing x)) possibilities in
---                any (\x -> all (== (head x) x)) filled--(all (== (head filled) ) filled)
+winner :: Board -> Maybe Piece
+winner b = head . head $ filter (\z -> all (== (head z)) z) (zones b)
+
+-- Display logic
+stringBoard :: Board -> String
+stringBoard b = format (map only b)
+       where only :: Maybe Piece -> String
+             only Nothing = " "
+             only (Just Cross) = "X"
+             only (Just Naught) = "O"
+             format :: [String] -> String
+             format [a,b,c,
+                     d,e,f,
+                     g,h,i] = a ++ " | " ++ b ++ " | " ++ c ++ "\n" ++
+                             "---------\n" ++
+                              d ++ " | " ++ e ++ " | " ++ f ++ "\n" ++
+                             "---------\n" ++
+                              g ++ " | " ++ h ++ " | " ++ i ++ "\n"
+
+printBoard :: Board -> IO ()
+printBoard = putStrLn . stringBoard
+
+main :: IO ()
+main = return ()
