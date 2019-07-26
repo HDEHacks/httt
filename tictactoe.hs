80fe07d59dc2fb38284f38fcd42199c7dd9c8e6f
Hedon <jdmaravelias@gmail.com>
2019-07-25 21:12:01 -0500

    Finish game
    Although game is functional, I would like to make several
    enhancements:
    
    1. AI to play against
    2. Use a state monad to simplify some of this logic
       For this, I will need to do quite a bit of reading :)

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,5 +1,6 @@
 data Piece = Cross | Naught deriving (Show, Eq)
 type Board = [Maybe Piece]
+data GameOver = Full | Piece deriving (Show)
 
 -- Board logic: this is monadic
 emptyBoard :: Board
@@ -23,7 +24,7 @@ winner b = head . head $ filter (\z -> all (== (head z)) z) (zones b)
 
 -- Display logic
 stringBoard :: Board -> String
-stringBoard b = format (map only b)
+stringBoard board = format (map only board)
        where only :: Maybe Piece -> String
              only Nothing = " "
              only (Just Cross) = "X"
@@ -40,5 +41,40 @@ stringBoard b = format (map only b)
 printBoard :: Board -> IO ()
 printBoard = putStrLn . stringBoard
 
+-- Play Logic
+whichTurn :: Bool -> String
+whichTurn True = "It's X's move."
+whichTurn False = "It's O's move."
+
+readMaybe :: String -> Maybe Int
+readMaybe s = case reads s of
+                  [(val, "")] -> Just val
+                  _           -> Nothing
+
+tryMove :: Board -> Piece -> Int -> IO ()
+tryMove board piece position = case putPiece board piece position of
+    Nothing -> do putStrLn "You can't play there."
+                  play board
+    Just newBoard -> case winner newBoard of
+                        Nothing -> play newBoard
+                        Just b -> let w = show b in
+                                  do printBoard newBoard
+                                     putStrLn $ w ++ " wins!"
+
+play :: Board -> IO ()
+play board = let turn       = length $ filter (/= Nothing) board
+                 isCatsGame = turn >= 9
+                 isXTurn    = turn `mod` 2 == 0
+                 whoseTurn  = if isXTurn then Cross else Naught
+             in if isCatsGame then (putStrLn "It's a tie!")
+                else do printBoard board
+                        putStrLn $ whichTurn isXTurn
+                        line <- getChar
+                        putStrLn ""
+                        case readMaybe [line] of
+                          Nothing -> do putStrLn "You can't play there."
+                                        play board
+                          Just position -> tryMove board whoseTurn position
+
 main :: IO ()
-main = return ()
+main = play emptyBoard
