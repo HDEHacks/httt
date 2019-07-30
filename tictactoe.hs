04c7db0b98a9fc97d5a17f4895107a9a05511a58
Hedon <jdmaravelias@gmail.com>
2019-07-29 21:49:08 -0500

    Clean functions

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,6 +1,5 @@
 data Piece = Cross | Naught deriving (Show, Eq)
 type Board = [Maybe Piece]
-data GameOver = Full | Piece deriving (Show)
 
 -- Board logic: this is monadic
 emptyBoard :: Board
@@ -10,7 +9,7 @@ putPiece :: Board -> Piece -> Int -> Maybe Board
 putPiece board piece n = if not (0 <= n && n < 9) then Nothing
     else case (board !! n) of 
         Just _ -> Nothing
-        Nothing -> Just $ (take n board) ++ [Just piece] ++ (drop (n + 1) board)
+        _ -> Just $ (take n board) ++ [Just piece] ++ (drop (n + 1) board)
 
 zones :: Board -> [[Maybe Piece]]
 zones [a,b,c,
@@ -20,15 +19,16 @@ zones [a,b,c,
                  [a,e,i], [g,e,c]]
 
 winner :: Board -> Maybe Piece
-winner b = head . head $ filter (\z -> all (== (head z)) z) (zones b)
+winner b = let winners = filter (\z -> all (== (head z)) z) (zones b)
+           in if length winners == 0 then Nothing else head . head $ winners
 
 -- Display logic
 stringBoard :: Board -> String
 stringBoard board = format (map only board)
        where only :: Maybe Piece -> String
-             only Nothing = " "
              only (Just Cross) = "X"
              only (Just Naught) = "O"
+             only _ = " "
              format :: [String] -> String
              format [a,b,c,
                      d,e,f,
@@ -42,39 +42,36 @@ printBoard :: Board -> IO ()
 printBoard = putStrLn . stringBoard
 
 -- Play Logic
-whichTurn :: Bool -> String
-whichTurn True = "It's X's move."
-whichTurn False = "It's O's move."
-
 readMaybe :: String -> Maybe Int
 readMaybe s = case reads s of
                   [(val, "")] -> Just val
                   _           -> Nothing
 
+announce :: Board -> Piece -> IO ()
+announce board winner = do printBoard board
+                           putStrLn $ (show winner) ++ " wins!"
+
 tryMove :: Board -> Piece -> Int -> IO ()
 tryMove board piece position = case putPiece board piece position of
-    Nothing -> do putStrLn "You can't play there."
-                  play board
-    Just newBoard -> case winner newBoard of
-                        Nothing -> play newBoard
-                        Just b -> let w = show b in
-                                  do printBoard newBoard
-                                     putStrLn $ w ++ " wins!"
+    Just next -> maybe (play next) (announce next) (winner next)
+    _ -> do putStrLn "You can't play there." ; play board
+
+getInt :: IO Int
+getInt = do line <- getChar
+            putStrLn ""
+            let parse = readMaybe [line]
+            let recurse = do putStrLn "That's not a number." ; getInt
+            maybe recurse pure parse
 
 play :: Board -> IO ()
 play board = let turn       = length $ filter (/= Nothing) board
-                 isCatsGame = turn >= 9
                  isXTurn    = turn `mod` 2 == 0
-                 whoseTurn  = if isXTurn then Cross else Naught
-             in if isCatsGame then (putStrLn "It's a tie!")
+                 whoseTurn  = if isXTurn then Cross else Naught 
+             in if turn >= 9 then putStrLn "It's a tie!"
                 else do printBoard board
-                        putStrLn $ whichTurn isXTurn
-                        line <- getChar
-                        putStrLn ""
-                        case readMaybe [line] of
-                          Nothing -> do putStrLn "You can't play there."
-                                        play board
-                          Just position -> tryMove board whoseTurn position
+                        putStrLn $ "It's " ++ (show whoseTurn) ++ "'s move."
+                        position <- getInt
+                        tryMove board whoseTurn position
 
 main :: IO ()
 main = play emptyBoard
